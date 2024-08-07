6727d720c3f0b37ef82807aaa9b19e81acd73c68
LordBugsy <iiomqitzswordenx@gmail.com>
2024-08-07 13:15:29 +0200

    updated components

--- src/Trades Component/Trades.jsx
+++ src/Trades Component/Trades.jsx
@@ -9,7 +9,7 @@ import Loading from '../Loading Component/Loading';
 const Trades = () => {
     const {setNotification} = useContext(DataContext);
 
-    const [tradeTab, setTradeTab] = useState(true);
+    const [searchTab, setTradeTab] = useState(true);
     const [searchResult, setSearchResult] = useState("");
     const [isButtonDisabled, setButtonState] = useState(true);
     const [isPlayerBrowsing, setBrowseState] = useState(false);
@@ -25,7 +25,7 @@ const Trades = () => {
     }
 
     const browsePlayers = () => {
-        if (userResult === "") {
+        if (userResult === "" && !isButtonDisabled) {
             setBrowseState(true);
             setTimeout(() => {
                 setUserResult("This feature hasn't been added yet.");
@@ -41,77 +41,77 @@ const Trades = () => {
                 <h1 className={styles.header}>Trades</h1>
     
                 <div className={styles.tabs}>
-                    <h1 className={styles.tab}>Search for trade</h1>
-                    <h1 className={styles.tab}>Trade requests</h1>
+                    <h1 onClick={() => setTradeTab(true)} className={`${styles.tab} ${searchTab ? styles.selectedTab : ''}`}>Search for trade</h1>
+                    <h1 onClick={() => setTradeTab(false)} className={`${styles.tab} ${!searchTab ? styles.selectedTab : ''}`}>Trade requests</h1>
                 </div>
 
-                <div className={styles.trades}>
-                    
-                    <div className={styles.searchTab}>
-                        <div className={styles.searchInput}>
-                            <input type='text' placeholder='Search a username..' className={styles.input} value={searchResult} onChange={() => searchPlayer(event)} />
-                            <button onClick={browsePlayers} disabled={isButtonDisabled} className={`${styles.responsive} ${styles.button} ${isButtonDisabled ? styles.disable : styles.searchPlayer}`}><i className="ri-search-2-line"></i></button>
+                { searchTab ? (
+                    <div id='tradeSearch' className={styles.tradeSearchTab}>
+                        <div className={styles.searchTab}>
+                            <div className={styles.searchInput}>
+                                <input id='input' type='text' placeholder='Search a username..' className={styles.input} value={searchResult} onChange={searchPlayer} />
+                                <button onClick={browsePlayers} disabled={isButtonDisabled} className={`${styles.responsive} ${styles.button} ${isButtonDisabled ? styles.disable : styles.searchPlayer}`}><i className="ri-search-2-line"></i></button>
+                            </div>
+
+                            <button onClick={browsePlayers} className={`${styles.hidden} ${styles.button} ${isButtonDisabled ? styles.disable : styles.searchPlayer}`}>Search</button>
+                            {isPlayerBrowsing && <Loading />}
+                            <h1 id='textResult' className={styles.searchResult}>{userResult}</h1>
                         </div>
-
-                        <button onClick={browsePlayers} className={`${styles.hidden} ${styles.button} ${isButtonDisabled ? styles.disable : styles.searchPlayer}`}>Search</button>
-                        {isPlayerBrowsing && <Loading />}
-                        <h1 id='textResult' className={styles.searchResult}>{userResult}</h1>
                     </div>
-
-                    {/* <div className={styles.tradeCard}>
-                        <div className={styles.tradeOffer}>
-                            <div className={styles.img}>
-                                <i className={`fa-solid fa-toolbox ${styles.icon}`}></i>
-                                <i className={`ri-arrow-left-right-line ${styles.tradeIcon}`}></i>
-                                <img className={styles.credit} src={credits} alt='coins' />
+                    )
+                        : 
+                    (
+                    <div id='tradeRequest' className={styles.trades}>
+                        <div className={styles.tradeCard}>
+                            <div className={styles.tradeOffer}>
+                                <div className={styles.img}>
+                                    <i className={`fa-solid fa-toolbox ${styles.icon}`}></i>
+                                    <i className={`ri-arrow-left-right-line ${styles.tradeIcon}`}></i>
+                                    <img className={styles.credit} src={credits} alt='coins' />
+                                </div>
+                            </div>
+                        
+                            <div className={styles.traderInfo}>
+                                <p><span className={styles.username}>John Doe</span> would like to trade!</p>
+                                <button className={`${styles.button} ${styles.accept}`}>See trade</button>
+                            </div>
+                        </div>
+                        
+                        <div className={styles.tradeCard}>
+                            <div className={styles.tradeOffer}>
+                                <div className={styles.img}>
+                                    <i className={`fa-solid fa-toolbox ${styles.icon}`}></i>
+                                    <i className={`ri-arrow-left-right-line ${styles.tradeIcon}`}></i>
+                                    <img className={styles.credit} src={credits} alt='coins' />
+                                </div>
+                            </div>
+                        
+                            <div className={styles.traderInfo}>
+                            <p><span className={styles.username}>LordBugsy</span> would like to trade!</p>
+                                <button className={`${styles.button} ${styles.accept}`}>See trade</button>
+                            </div>
+                        </div>
+                        
+                        <div className={styles.tradeCard}>
+                            <div className={styles.tradeOffer}>
+                                <div className={styles.img}>
+                                    <i className={`fa-solid fa-toolbox ${styles.icon}`}></i>
+                                    <i className={`ri-arrow-left-right-line ${styles.tradeIcon}`}></i>
+                                    <img className={styles.credit} src={credits} alt='coins' />
+                                </div>
+                            </div>
+                        
+                            <div className={styles.traderInfo}>
+                                <p><span className={styles.username}>Dummy</span> would like to trade!</p>
+                                <button className={`${styles.button} ${styles.accept}`}>See trade</button>
                             </div>
                         </div>
 
-                        <div className={styles.traderInfo}>
-                            <p><span className={styles.username}>Dummy</span> offers for <span className={styles.span}>50 of your credits!</span></p>
-                            <button className={`${styles.button} ${styles.accept}`}>Accept</button>
-                        </div>
                     </div>
+                )}
 
-                    <div className={styles.tradeCard}>
-                        <div className={styles.tradeOffer}>
-                            <div className={styles.img}>
-                                <i className={`fa-solid fa-toolbox ${styles.icon}`}></i>
-                                <i className={`ri-arrow-left-right-line ${styles.tradeIcon}`}></i>
-                                <img className={styles.credit} src={credits} alt='coins' />
-                            </div>
-                        </div>
-
-                        <div className={styles.traderInfo}>
-                            <p><span className={styles.username}>Dummy</span> offers for <span className={styles.span}>50 of your credits!</span></p>
-                            <button className={`${styles.button} ${styles.accept}`}>Accept</button>
-                        </div>
-                    </div>
-
-                    <div className={styles.tradeCard}>
-                        <div className={styles.tradeOffer}>
-                            <div className={styles.img}>
-                                <i className={`fa-solid fa-toolbox ${styles.icon}`}></i>
-                                <i className={`ri-arrow-left-right-line ${styles.tradeIcon}`}></i>
-                                <img className={styles.credit} src={credits} alt='coins' />
-                            </div>
-                        </div>
-
-                        <div className={styles.traderInfo}>
-                            <p><span className={styles.username}>Dummy</span> offers for <span className={styles.span}>50 of your credits!</span></p>
-                            <button className={`${styles.button} ${styles.accept}`}>Accept</button>
-                        </div>
-                    </div>
-
-                     */}
-
-                </div>
             </div>
 
-            {/* <div className={styles.newTrade}>
-                <button className={`${styles.button} ${styles.traderequest}`}>Get New Trades</button>
-            </div> */}
-            
             <div className='copyright'>
                 <h2>*STILL IN DEVELOPMENT*</h2>
                 <p>Copyright © 2024 LordBugsy. All Rights Reserved.</p>
